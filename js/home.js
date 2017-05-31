a69b52d4167095a46c0d43b2807de0cc53329451
tyler2120253 <ghassan714@gmail.com>
2017-05-31 12:30:42 +0300

    hi there

---
+++ js/home.js
@@ -0,0 +1,10 @@
+var app = angular.module("app.home", ["ngRoute"]);
+app.config(function ($routeProvider) {
+    $routeProvider.when("/", {
+        templateUrl: "./veiw/home.html",
+        controller: "homeCtrl"
+    })
+})
+app.controller("homeCtrl", function ($scope) {
+    
+})
